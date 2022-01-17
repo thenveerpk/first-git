3e2ef79a2896e24d5f0f01d1d7eaae6914eb0302
thenveerpk <thanveerpk912@gmail.com>
2022-01-17 12:30:42 +0530

    first commit

---
+++ bubblesort4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int a[50],i,j,temp,size;
+	printf("enter the size");
+	scanf("%d",&size);
+	printf("enter the elements of array:");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<size;j++)
+		{
+
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	printf("the sorted array is");
+	for(i=0;i<size;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+	return 0;
+}
+	
+	
